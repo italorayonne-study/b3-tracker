b1b4def9f35b4d5259267e22a49313c4e27dd4f2
rayoneitalo <rayoneitalo@gmail.com>
2024-11-27 13:30:05 +0000

    draft code

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,41 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, Image, StatusBar, Text, View } from "react-native";
 
 export default function Index() {
+  const [isLoading, setIsLoagind] = useState(true)
+  const [error, setError] = useState()
+  const [response, setResponse] = useState()
+
+  useEffect(() => {
+    fetch('https://brapi.dev/api/quote/list?search=VALE3&limit=2', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer popeYQfXMTauF2XpUzobK4d',
+      }
+    })
+      .then(response => response.json())
+      .then((result) => {
+        setIsLoagind(false);
+        setResponse(result)
+      }, (error) => {
+        setIsLoagind(false)
+        setError(error)
+      })
+  }, [])
+
+  const getContent = () => {
+
+    if (isLoading) return <ActivityIndicator size={"large"} />
+
+    if(error) return <Text>{error}</Text>
+
+    return <Image src="https://s3-symbol-logo.tradingview.com/vale--big.svg"></Image>
+    // return <Image>{response["stocks"][0]["close"]}</Text>
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-slate-500">
-      <Text className="text-white">Hello B Trackers!</Text>
-    </View>
+    <View className="flex-1 items-center justify-center">
+      {getContent()}
+    </View> 
   );
 }
